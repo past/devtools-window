99d8493a723cc50c8672c71e18f6e90c1e29c53b
MikeRatcliffe <mratcliffe@mozilla.com>
2012-10-18 16:58:02 +0100

    Added test browser_new_activation_workflow.js

--- browser/devtools/framework/test/browser_new_activation_workflow.js
+++ browser/devtools/framework/test/browser_new_activation_workflow.js
@@ -5,22 +5,59 @@
 
 const Cu = Components.utils;
 
+let toolbox;
+
 function test() {
   addTab("about:blank", function(aBrowser, aTab) {
-    runTests(aTab);
+    loadWebConsole(aTab);
   });
 }
 
-function runTests(aTab) {
+function loadWebConsole(aTab) {
   ok(gDevTools, "gDevTools exists");
 
+  let target = {
+    type: gDevTools.TargetType.TAB,
+    value: gBrowser.selectedTab
+  };
+  toolbox = gDevTools.openToolbox(target, "bottom", "webconsole");
+  toolbox.once("load", checkToolLoading);
+}
+
+function checkToolLoading() {
+  is(toolbox.currentToolId, "webconsole", "The web console is selected");
+  selectAndCheckById("jsdebugger");
+  selectAndCheckById("styleeditor");
+  testToggle();
+}
+
+function selectAndCheckById(id) {
+  let doc = toolbox.frame.contentDocument;
+
+  toolbox.selectTool(id);
+  let tab = doc.getElementById("toolbox-tab-" + id);
+  is(tab.selected, true, "The " + id + " tab is selected");
+}
+
+function testToggle() {
+  toolbox.once("destroyed", function() {
+    let target = {
+      type: gDevTools.TargetType.TAB,
+      value: gBrowser.selectedTab
+    };
+    toolbox = gDevTools.openToolbox(target, "bottom", "styleeditor");
+    toolbox.once("load", checkStyleEditorLoaded);
+  }.bind(this));
+
+  gDevTools.toggleToolboxForTab(gBrowser.selectedTab);
+}
+
+function checkStyleEditorLoaded() {
+  is(toolbox.currentToolId, "styleeditor", "The style editor is selected");
   finishUp();
 }
 
 function finishUp() {
-  let toolBoxes = gDevTools.getToolBoxes(gBrowser.selectedTab);
-  let toolbox = toolBoxes.get(gBrowser.selectedTab);
-
   toolbox.destroy();
   toolbox = null;
   gBrowser.removeCurrentTab();
